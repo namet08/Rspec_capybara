44570fb226a1c338deb76f9fbfdbea7740621566
namet08 <93746140+namet08@users.noreply.github.com>
2022-08-19 17:35:04 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,6 +13,5 @@ end
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome_headless
   config.default_max_wait_time = 15
-  config.page.driver.browser.manage.window.maximize 
   config.app_host = "https://portal.goflux.com.br"
 end
