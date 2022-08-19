0f4a65bee8a383ad5d003892d6bf5fc0dab5a550
namet08 <93746140+namet08@users.noreply.github.com>
2022-08-19 17:42:14 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@ require "rufo"
 require "rspec"
 require "capybara/rspec"
 require "ffi"
+require "allure-rspec"
 require "site_prism"
 require_relative "../pages/register_page.rb"
 
@@ -14,4 +15,10 @@ Capybara.configure do |config|
   config.default_driver = :selenium_chrome_headless
   config.default_max_wait_time = 15
   config.app_host = "https://portal.goflux.com.br"
+end
+
+
+AllureRspec.configure do |config|
+  config.results_directory = "report/allure-results"
+  config.clean_results_directory = true
 end
